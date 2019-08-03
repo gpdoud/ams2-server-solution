860ec8a8f28ea5ebfd83f7988bb628af28c837a3
Greg Doud <gpdoud@gmail.com>
2019-08-03 19:27:59 -0400

    #57 Trap back-end exceptions 1) JsonResponse: Added MethodName and FormattedMessage 2) JsonResponse: Added CreateJsonResponseExceptionInstance() that instantiates an instance of JsonResponse setup to handle exceptions 3) Added try .. catch block to the bodies of GetVehicle, PutVehicle, PostVehicle, and DeleteVehicle

--- Ams2PrototypeProject/Controllers/EquipmentsController.cs
+++ Ams2PrototypeProject/Controllers/EquipmentsController.cs
@@ -10,88 +10,108 @@ using System.Web.Http.Cors;
 
 namespace Ams2.Controllers {
 
-	[EnableCors(origins: "*", headers: "*", methods: "*")]
-	public class EquipmentsController : AmsWebApiController {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EquipmentsController : AmsWebApiController {
 
-		//private AmsDbContext db = new AmsDbContext();
+        //private AmsDbContext db = new AmsDbContext();
 
-		[HttpGet]
-		[ActionName("List")]
-		public JsonResponse GetEquipment() {
-			return new JsonResponse(db.Equipments.ToList());
-		}
+        [HttpGet]
+        [ActionName("List")]
+        public JsonResponse GetEquipment() {
+            return new JsonResponse(db.Equipments.ToList());
+        }
 
-		[HttpGet]
-		[ActionName("Get")]
-		public JsonResponse GetEquipment(int? id) {
-			if (id == null)
-				return new JsonResponse { Code = -2, Message = "Parameter id cannot be null" };
-			var equipment = db.Equipments.Find(id);
-			if(equipment == null)
-				return new JsonResponse { Code = -2, Message = $"Equipment id={id} not found" };
-			return new JsonResponse(equipment); 
-		}
+        [HttpGet]
+        [ActionName("Get")]
+        public JsonResponse GetEquipment(int? id) {
+            try {
+                if(id == null)
+                    return new JsonResponse { Code = -2, Message = "Parameter id cannot be null" };
+                var equipment = db.Equipments.Find(id);
+                if(equipment == null)
+                    return new JsonResponse { Code = -2, Message = $"Equipment id={id} not found" };
+                return new JsonResponse(equipment);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
 
-		[HttpPost]
-		[ActionName("Create")]
-		public JsonResponse CreateEquipment([FromBody] Equipment equipment) {
-			if (equipment == null)
-				return new JsonResponse { Code = -2, Message = "Parameter equipment cannot be null" };
-			if (!ModelState.IsValid)
-				return new JsonResponse { Code = -1, Message = "ModelState invalid", Error = ModelState };
-			// add the asset first
-			// needs all the asset data entered already
-			var asset = db.Assets.Add(equipment.Asset);
-			var recsAffected = db.SaveChanges(); // so the asset exists for the equipment
-			if (recsAffected != 1)
-				return new JsonResponse { Code = -2, Message = "Create asset failed while attempting to add equipment" };
-			equipment.AssetId = asset.Id; // this gets the generated PK
-			equipment.DateCreated = DateTime.Now;
-			db.Equipments.Add(equipment);
-			var resp = new JsonResponse { Message = "Equipment Created", Data = equipment };
-			return SaveChanges(resp);
-		}
+        [HttpPost]
+        [ActionName("Create")]
+        public JsonResponse CreateEquipment([FromBody] Equipment equipment) {
+            try {
+                if(equipment == null)
+                    return new JsonResponse { Code = -2, Message = "Parameter equipment cannot be null" };
+                if(!ModelState.IsValid)
+                    return new JsonResponse { Code = -1, Message = "ModelState invalid", Error = ModelState };
+                // add the asset first
+                // needs all the asset data entered already
+                var asset = db.Assets.Add(equipment.Asset);
+                var recsAffected = db.SaveChanges(); // so the asset exists for the equipment
+                if(recsAffected != 1)
+                    return new JsonResponse { Code = -2, Message = "Create asset failed while attempting to add equipment" };
+                equipment.AssetId = asset.Id; // this gets the generated PK
+                equipment.DateCreated = DateTime.Now;
+                db.Equipments.Add(equipment);
+                var resp = new JsonResponse { Message = "Equipment Created", Data = equipment };
+                return SaveChanges(resp);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
 
-		[HttpPost]
-		[ActionName("Change")]
-		public JsonResponse ChangeEquipment([FromBody] Equipment equipment) {
-			if (equipment == null)
-				return new JsonResponse { Code = -2, Message = "Parameter equipment cannot be null" };
-			// issue #11
-			// If the addressId in the asset is set to null (clears the address dropdown)
-			// set the Asset instance to null also.
-			//if (equipment.Asset.AddressId == null)
-			//	equipment.Asset.Address = null;
-			ClearAssetVirtuals(equipment);
-			if (!ModelState.IsValid)
-				return new JsonResponse { Code = -1, Message = "ModelState invalid", Error = ModelState };
-			equipment.DateUpdated = DateTime.Now;
-			db.Entry(equipment.Asset).State = System.Data.Entity.EntityState.Modified;
-			db.Entry(equipment).State = System.Data.Entity.EntityState.Modified;
-			var resp = new JsonResponse { Message = "Equipment Changed", Data = equipment };
-			return SaveChanges(resp);
-		}
+        [HttpPost]
+        [ActionName("Change")]
+        public JsonResponse ChangeEquipment([FromBody] Equipment equipment) {
+            try {
+                if(equipment == null)
+                    return new JsonResponse { Code = -2, Message = "Parameter equipment cannot be null" };
+                // issue #11
+                // If the addressId in the asset is set to null (clears the address dropdown)
+                // set the Asset instance to null also.
+                //if (equipment.Asset.AddressId == null)
+                //	equipment.Asset.Address = null;
+                ClearAssetVirtuals(equipment);
+                if(!ModelState.IsValid)
+                    return new JsonResponse { Code = -1, Message = "ModelState invalid", Error = ModelState };
+                equipment.DateUpdated = DateTime.Now;
+                db.Entry(equipment.Asset).State = System.Data.Entity.EntityState.Modified;
+                db.Entry(equipment).State = System.Data.Entity.EntityState.Modified;
+                var resp = new JsonResponse { Message = "Equipment Changed", Data = equipment };
+                return SaveChanges(resp);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
 
-		[HttpPost]
-		[ActionName("Remove")]
-		public JsonResponse RemoveEquipment([FromBody] Equipment equipment) {
-			if (equipment == null)
-				return new JsonResponse { Code = -2, Message = "Parameter equipment cannot be null" };
-			db.Entry(equipment.Asset).State = System.Data.Entity.EntityState.Deleted;
-			// the related equipment record will be deleted also because
-			// of cascading delete
-			var resp = new JsonResponse { Message = "Equipment Removed", Data = equipment };
-			return SaveChanges(resp);
-		}
+        [HttpPost]
+        [ActionName("Remove")]
+        public JsonResponse RemoveEquipment([FromBody] Equipment equipment) {
+            try {
+                if(equipment == null)
+                    return new JsonResponse { Code = -2, Message = "Parameter equipment cannot be null" };
+                db.Entry(equipment.Asset).State = System.Data.Entity.EntityState.Deleted;
+                // the related equipment record will be deleted also because
+                // of cascading delete
+                var resp = new JsonResponse { Message = "Equipment Removed", Data = equipment };
+                return SaveChanges(resp);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
 
-		//private JsonResponse SaveChanges(JsonResponse resp) {
-		//	try {
-		//		db.SaveChanges();
-		//		return resp ?? JsonResponse.Ok;
-		//	} catch (Exception ex) {
-		//		return new JsonResponse { Message = ex.Message, Error = ex };
-		//	}
-		//}
+        //private JsonResponse SaveChanges(JsonResponse resp) {
+        //	try {
+        //		db.SaveChanges();
+        //		return resp ?? JsonResponse.Ok;
+        //	} catch (Exception ex) {
+        //		return new JsonResponse { Message = ex.Message, Error = ex };
+        //	}
+        //}
 
-	}
+    }
 }
