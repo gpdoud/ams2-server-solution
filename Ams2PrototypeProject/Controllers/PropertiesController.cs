2f089f4903b007371aef6a508cc37b4f5b6511bf
Greg Doud <gpdoud@gmail.com>
2019-08-03 19:57:27 -0400

    #57 Trap back-end exceptions 1) JsonResponse: Added MethodName and FormattedMessage 2) Added try .. catch block to the bodies of GetProperty, PutProperty, PostProperty, and DeleteProperty

--- Ams2PrototypeProject/Controllers/PropertiesController.cs
+++ Ams2PrototypeProject/Controllers/PropertiesController.cs
@@ -10,77 +10,97 @@ using System.Web.Http.Cors;
 
 namespace Ams2.Controllers {
 
-	[EnableCors(origins: "*", headers: "*", methods: "*")]
-	public class PropertiesController : AmsWebApiController {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class PropertiesController : AmsWebApiController {
 
-		[HttpGet]
-		[ActionName("List")]
-		public JsonResponse GetProperty() {
-			return new JsonResponse(db.Properties.ToList());
-		}
+        [HttpGet]
+        [ActionName("List")]
+        public JsonResponse GetProperty() {
+            return new JsonResponse(db.Properties.ToList());
+        }
 
-		[HttpGet]
-		[ActionName("Get")]
-		public JsonResponse GetProperty(int? id) {
-			if (id == null)
-				return new JsonResponse { Message = "Parameter id cannot be null" };
-			var property = db.Properties.Find(id);
-			if (property == null)
-				return new JsonResponse { Message = $"Property id={id} not found" };
-			return new JsonResponse(property);
-		}
+        [HttpGet]
+        [ActionName("Get")]
+        public JsonResponse GetProperty(int? id) {
+            try {
+                if(id == null)
+                    return new JsonResponse { Message = "Parameter id cannot be null" };
+                var property = db.Properties.Find(id);
+                if(property == null)
+                    return new JsonResponse { Message = $"Property id={id} not found" };
+                return new JsonResponse(property);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
 
-		[HttpPost]
-		[ActionName("Create")]
-		public JsonResponse CreateProperty([FromBody] Property property) {
-			if (property == null)
-				return new JsonResponse { Message = "Parameter property cannot be null" };
-			if (!ModelState.IsValid)
-				return new JsonResponse { Message = "ModelState invalid", Error = ModelState };
-			// add the asset first
-			// needs all the asset data entered already
-			var asset = db.Assets.Add(property.Asset);
-			var recsAffected = db.SaveChanges(); // so the asset exists for the property
-			if (recsAffected != 1)
-				return new JsonResponse("Create asset failed while attempting to add property");
-			property.AssetId = asset.Id; // this gets the generated PK
-			property.DateCreated = DateTime.Now;
-			db.Properties.Add(property);
-			var resp = new JsonResponse { Message = "Property Created", Data = property };
-			return SaveChanges(resp);
-		}
+        [HttpPost]
+        [ActionName("Create")]
+        public JsonResponse CreateProperty([FromBody] Property property) {
+            try {
+                if(property == null)
+                    return new JsonResponse { Message = "Parameter property cannot be null" };
+                if(!ModelState.IsValid)
+                    return new JsonResponse { Message = "ModelState invalid", Error = ModelState };
+                // add the asset first
+                // needs all the asset data entered already
+                var asset = db.Assets.Add(property.Asset);
+                var recsAffected = db.SaveChanges(); // so the asset exists for the property
+                if(recsAffected != 1)
+                    return new JsonResponse("Create asset failed while attempting to add property");
+                property.AssetId = asset.Id; // this gets the generated PK
+                property.DateCreated = DateTime.Now;
+                db.Properties.Add(property);
+                var resp = new JsonResponse { Message = "Property Created", Data = property };
+                return SaveChanges(resp);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
 
-		[HttpPost]
-		[ActionName("Change")]
-		public JsonResponse ChangeProperty([FromBody] Property property) {
-			if (property == null)
-				return new JsonResponse { Message = "Parameter property cannot be null" };
-			// issue #11
-			// If the addressId in the asset is set to null (clears the address dropdown)
-			// set the Asset instance to null also.
-			//if (property.Asset.AddressId == null)
-			//	property.Asset.Address = null;
-			ClearAssetVirtuals(property);
-			
-			if (!ModelState.IsValid)
-				return new JsonResponse { Message = "ModelState invalid", Error = ModelState };
-			property.DateUpdated = DateTime.Now;
-			db.Entry(property.Asset).State = System.Data.Entity.EntityState.Modified;
-			db.Entry(property).State = System.Data.Entity.EntityState.Modified;
-			var resp = new JsonResponse { Message = "Property Changed", Data = property };
-			return SaveChanges(resp);
-		}
+        [HttpPost]
+        [ActionName("Change")]
+        public JsonResponse ChangeProperty([FromBody] Property property) {
+            try {
+                if(property == null)
+                    return new JsonResponse { Message = "Parameter property cannot be null" };
+                // issue #11
+                // If the addressId in the asset is set to null (clears the address dropdown)
+                // set the Asset instance to null also.
+                //if (property.Asset.AddressId == null)
+                //	property.Asset.Address = null;
+                ClearAssetVirtuals(property);
 
-		[HttpPost]
-		[ActionName("Remove")]
-		public JsonResponse RemoveProperty([FromBody] Property property) {
-			if (property == null)
-				return new JsonResponse { Message = "Parameter property cannot be null" };
-			db.Entry(property.Asset).State = System.Data.Entity.EntityState.Deleted;
-			// the related property record will be deleted also because
-			// of cascading delete
-			var resp = new JsonResponse { Message = "Property Removed", Data = property };
-			return SaveChanges(resp);
-		}
-	}
+                if(!ModelState.IsValid)
+                    return new JsonResponse { Message = "ModelState invalid", Error = ModelState };
+                property.DateUpdated = DateTime.Now;
+                db.Entry(property.Asset).State = System.Data.Entity.EntityState.Modified;
+                db.Entry(property).State = System.Data.Entity.EntityState.Modified;
+                var resp = new JsonResponse { Message = "Property Changed", Data = property };
+                return SaveChanges(resp);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Remove")]
+        public JsonResponse RemoveProperty([FromBody] Property property) {
+            try {
+                if(property == null)
+                    return new JsonResponse { Message = "Parameter property cannot be null" };
+                db.Entry(property.Asset).State = System.Data.Entity.EntityState.Deleted;
+                // the related property record will be deleted also because
+                // of cascading delete
+                var resp = new JsonResponse { Message = "Property Removed", Data = property };
+                return SaveChanges(resp);
+            } catch(Exception ex) {
+                var jr = JsonResponse.CreateJsonResponseExceptionInstance(-999, $"EXCEPTION: {ex.Message}", ex);
+                return jr;
+            }
+        }
+    }
 }
